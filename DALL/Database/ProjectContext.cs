4ba1bdd5cfc6d4a4a41748f7e86996a979f566cd
Hamza KECELİ <hamzakeceli52@gmail.com>
2019-12-29 16:18:44 +0300

    Dall ve entity added

---
+++ DALL/Database/ProjectContext.cs
@@ -0,0 +1,49 @@
+﻿using DALL.Mapping;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL.Database
+{
+    public class ProjectContext:DbContext
+    {
+
+
+
+        public ProjectContext()
+           : base("defaultConnection")  //wepconfig alıyor
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+             modelBuilder.Configurations.Add(new CategoryMapping());
+             modelBuilder.Configurations.Add(new ProductMapping());
+
+            //s takısı olmadan tablo yarat
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Category> MyProperty { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Product            
+    {
+
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        public string ProductImage { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+    }
+}
